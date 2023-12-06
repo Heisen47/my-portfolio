8a9ab94bd90744dbb544d18eafb0b115687c69ea
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2023-12-06 19:26:34 +0530

    typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Rishii</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I enjoy building apps in web mostly in  <i>React</i> <br className='sm:block hidden' />
+            I enjoy building apps in web mostly in <i>React</i> <br className='sm:block hidden' />
             An avid learner currently learning FullStack development
           </p>
         </div>
